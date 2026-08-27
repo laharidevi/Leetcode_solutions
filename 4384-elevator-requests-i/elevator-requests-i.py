b568f07ef2f812d1372da5227f54103a9fc39223
Lahari Devi <laharidevi195@gmail.com>
2026-08-27 10:37:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (74.28%) - LeetSync

---
+++ 4384-elevator-requests-i/elevator-requests-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def elevatorRequests(self, n: int, requests: list[int]) -> int:
+        current = 0
+        total = 0
+
+        for floor in requests:
+            total += abs(floor - current)
+            current = floor
+
+        return total
+        
